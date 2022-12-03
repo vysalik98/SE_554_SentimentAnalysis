bf5e8d8e5e3143825c1ca0c9f769294ac7d78cf1
Prachi Kashyap <114448290+prachi-kashyap@users.noreply.github.com>
2022-12-02 20:54:56 -0800

    Update test_read_csv.py

--- unit_test/test_read_csv.py
+++ unit_test/test_read_csv.py
@@ -20,16 +20,16 @@ rows = [
 class TestCsv(unittest.TestCase):
 
     def setUp(self):
-        with open(test_file, 'w', newline='') as csv_file:
-            writer = csv.writer(csv_file, dialect='excel')
+        with open(test_file, 'w', newline='') as readcsv:
+            writer = csv.writer(readcsv, dialect='excel')
             writer.writerows(rows)
 
     def tearDown(self):
         os.remove(test_file)
 
     def test_read_line(self):
-        with open(test_file, 'r') as csv_file:
-            reader = csv.reader(csv_file, dialect='excel')
+        with open(test_file, 'r') as readcsv:
+            reader = csv.reader(readcsv, dialect='excel')
             self.assertEqual(next(reader), rows[0])
             self.assertEqual(next(reader), rows[1])
 
